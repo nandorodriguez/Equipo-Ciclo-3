2dcb884127ef62e9b4d5c2900b0c7b9126d816a1
Samuel Cerón <est.wilder.taborda@unimilitar.edu.co>
2021-09-23 18:24:01 -0500

    creando carpetas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Login from '../components/samuel/Login';
+import "./App.css";
+import Login from "../components/samuel/Login";
+import Carrito from "../components/fernando/Carrito";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Carrito />
     </div>
   );
 }
