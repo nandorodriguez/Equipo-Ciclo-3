8f8a82df559ab79ffd4d9aae9e64145ef3cdd082
YilmarGarces <gilmar.garces@gmail.com>
2021-09-26 08:39:11 -0500

    creando estilo

--- client/components/yilmar/usuarios.js
+++ client/components/yilmar/usuarios.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Usuarios = () => {
     return (
         <div>
-           
+           Usuarios <input type="button" value="nuevo" />
+            <input type="search" name="buscar" value="" />
+            <input type="button" value="Buscar" />
+            <br />
+            <br />
+            | Todos | Administrador | Editor | Otros |
+            <br />
+            <div>
+                <input type="button" value="Editar" />
+                <input type="button" value="Eliminar" />
+                <input type="button" value="Cambiar rol" />
+                <input type="button" value="Ver" />
+            </div>
+            <div>
+                <input type="checkbox" value="nombre de usuario" title="nombre de usuario " /> Nombre de usuario | Nombres y apellidos | Correo electronico | Perfil <br />
+            </div>
+  
         </div>
     )
 }
