fe70f92e630196a2f81a9b2a339e7eeacce64ee1
YilmarGarces <gilmar.garces@gmail.com>
2021-09-26 08:47:32 -0500

    dandoestilo

--- client/components/yilmar/usuarios.js
+++ client/components/yilmar/usuarios.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 const Usuarios = () => {
     return (
         <div>
-           Usuarios <input type="button" value="nuevo" />
-            <input type="search" name="buscar" value="" />
-            <input type="button" value="Buscar" />
+           Usuarios <Button variant="contained">Nuevo</Button>
+           <TextField id="standard-basic" label="Standard" variant="standard" />
+           <Button variant="outlined">Buscar</Button>
             <br />
             <br />
-            | Todos | Administrador | Editor | Otros |
+            <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            >
+            <BottomNavigationAction label="Todos" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Administradores" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Editores" icon={<LocationOnIcon />} />
+            </BottomNavigation>
+        
             <br />
             <div>
-                <input type="button" value="Editar" />
-                <input type="button" value="Eliminar" />
-                <input type="button" value="Cambiar rol" />
-                <input type="button" value="Ver" />
+           
+                <Button variant="contained">Editar</Button>;
+                <Button variant="contained">Eliminar</Button>;
+                <Button variant="contained">Ver</Button>;
+                <Button variant="contained">Cambiar Rol</Button>;
             </div>
             <div>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Nombre de usuario" />
+                <FormControlLabel disabled control={<Checkbox />} label="nombres y ape" />
+            </FormGroup>
+
                 <input type="checkbox" value="nombre de usuario" title="nombre de usuario " /> Nombre de usuario | Nombres y apellidos | Correo electronico | Perfil <br />
             </div>
   
