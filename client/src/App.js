e7bc8bb01f937ff5a0728391ab01321c5b46e3bb
Samuel Cerón <est.wilder.taborda@unimilitar.edu.co>
2021-09-27 18:48:34 -0500

    agregando administrador de estados

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
 import Login from "./components/samuel/Login";
 import Producto from "./components/fernandoZuky/Producto";
 import Usuarios from "./components/yilmar/usuarios";
+import VentasModulo from "./components/samuel/VentasModulo";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser,handleLogin } from "./features/userSlice";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <VentasModulo />
       <Producto />
       <Usuarios />
     </div>
--- client/src/components/samuel/Login.js
+++ client/src/components/samuel/Login.js
@@ -1,109 +1,19 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Paper from "@mui/material/Paper";
+import React from "react";
 import GoogleIcon from "@mui/icons-material/Google";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import "./Login.css";
 
-const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("send");
-  };
-
+export default function ColorButtons() {
   return (
-    <>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          component={Paper}
-          elevation={10}
-          square
-        >
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="outlined"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                startIcon={<GoogleIcon />}
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Google
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </>
+    <div className="login">
+      <Button
+        className="login__btn"
+        variant="contained"
+        color="error"
+        startIcon={<GoogleIcon />}
+      >
+        Google
+      </Button>
+    </div>
   );
-};
-
-export default Login;
+}
