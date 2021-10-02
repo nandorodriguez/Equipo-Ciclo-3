e69fcb407b3c871ac131b5ddcea9192c0f8f7eed
Samuel Cerón <est.wilder.taborda@unimilitar.edu.co>
2021-10-02 16:53:39 -0500

    adding items

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
